376d3efe9819b069237b5389df81c1edd74c7d7a
amit369 <amitchaman369@gmail.com>
2023-06-23 22:02:59 +0530

    added commands

---
+++ commands.js
@@ -0,0 +1,11 @@
+// MySQL Commands in Ubuntu 
+
+1. sudo apt install mysql-server
+2. show databases
+3. use database_name
+4. show tables
+5. create table first_table (name varchar(20) , email varchar(20));
+6. describe first_table
+7. insert into first_table value ('Amit','a@gmail.com')
+8. delete from first_table
+9. truncate table first_table
